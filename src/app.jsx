c84e12e2dcffdf9b27a259f73293ba5d703fb3c6
Evgeny Bobkin <evgeny.bobkin@stackfuel.com>
2020-04-04 22:16:08 +0200

    first commit

---
+++ src/app.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+// import logo from "./logo.svg"
+// import "./App.css"
+
+import BarChart from "./components/bar-chart"
+
+function App() {
+  return (
+    <div>
+      <h2>App</h2>
+      <BarChart />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/bar-chart.jsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+const BarChart = () => <div>Bar Chart</div>
+
+export default BarChart
